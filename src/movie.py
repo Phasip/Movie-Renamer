0c1f662cb4a05c3d581a6ad5ef29f49ed0818393
Pasi Saarinen <saarinen.pasi@gmail.com>
2013-03-11 21:33:05 +0100

    Don't crash if we don't have any results

--- src/movie.py
+++ src/movie.py
@@ -1,5 +1,4 @@
 import imdb
-
 class Movie(object):
     def __init__(self, title, year):
         self.title = title
@@ -15,6 +14,9 @@ class Movie(object):
     @staticmethod
     def create_movie_from_imdb_query(query):
         db = imdb.IMDb()
-        movie_obj = db.search_movie(query, results=1)[0]
+        movie_list = db.search_movie(query, results=1)
+        if len(movie_list) == 0:
+		    return Movie(title="None", year="0")
+        movie_obj = movie_list[0]
         created_movie = Movie(title=movie_obj['title'], year=movie_obj['year'])
         return created_movie
