37b65a066a98a6fe3d1e5ba1715a897155f3829b
Pasi Saarinen <saarinen.pasi@gmail.com>
2013-03-11 21:35:05 +0100

    Multiple fixes
    Not checking extension of folder,
    Returning None if no results
    Convert movie with utf-8 to avoid ascii errors

--- src/movie_renamer.py
+++ src/movie_renamer.py
@@ -1,9 +1,19 @@
 from movie import Movie
 from imdbquery import ImdbQuery
 import os
+import sys
 
 def rename(filename):
-    filename_without_extension, extension = os.path.splitext(filename)
-    query = ImdbQuery.create_query_from_filename(filename_without_extension)
-    movie = Movie.create_movie_from_imdb_query(query)
-    return str(movie) + extension
+	filename_without_extension = filename
+	extension = ""
+	
+	if os.path.isdir(filename) == False: 
+		filename_without_extension, extension = os.path.splitext(filename)
+	    
+	query = ImdbQuery.create_query_from_filename(filename_without_extension)
+	movie = Movie.create_movie_from_imdb_query(query)
+	if movie.title == "None":
+#		sys.stdout.write("Query Was: " + query + "\n")
+		return "None"
+		
+	return unicode(movie).encode("utf-8") + extension
